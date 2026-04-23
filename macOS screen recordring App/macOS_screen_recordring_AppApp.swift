d51888de2236affbf526e138dc3bd181898e8d8a
namangupta44 <namank44.44@gmail.com>
2026-04-23 18:31:56 +0530

    Initial Commit

---
+++ macOS screen recordring App/macOS_screen_recordring_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOS_screen_recordring_AppApp.swift
+//  macOS screen recordring App
+//
+//  Created by Naman Gupta on 23/04/26.
+//
+
+import SwiftUI
+
+@main
+struct macOS_screen_recordring_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
